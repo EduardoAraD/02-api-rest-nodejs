989f220e94556d662fa2162c14d2d659e1564fcd
eduardoarad <araujocarlos893@gmail.com>
2024-01-17 17:24:17 -0300

    Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello Node'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .where('amount', 100)
+    .select('*')
+
+  // .select('*')
+
+  // .insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Transação de testes',
+  //   amount: 1000,
+  // })
+  // .returning('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running')
